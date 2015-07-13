e2f1093cadcc5227731033c538593674ac9ec96f
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-13 00:24:50 -0400

    1

---
+++ practice_expr.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Miss_Jessica_Lynne on 7/12/15.
+ */
+
+//basic math exp addition
+var a;
+a = 2;
+var b;
+b = a + 2;
+alert(b);
+
+//example of subtraction
+var yearBorn = 1974;
+var age = 2012 - yearBorn;
+console.log (age);
+
+//mult and divi example = area of a triangel is half the width xs the height. w * h / 2=
+
+var width = 4;
+var height = 5;
+var area = width * height / 2;
+console.log(area);
+
+//modulo gives you the remainder or whats left over of a number
+var remainder = 32 % 10;
+console.log(remainder);
+
+
---
+++ simple.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Miss_Jessica_Lynne on 7/12/15.
+ */
+
+var firstnumber = prompt("Give me a number.", "");
+var secondnumber = prompt("Give me another number.", "");
+var total = firstnumber * secondnumber;
+alert(firstnumber + " x " + secondnumber + " = " + total);
